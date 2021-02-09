53e406a1b4bd066c80e722d0582fa2fafe265667
Jeremy Guler <bubulldog20@yahoo.com>
2021-02-08 21:07:53 -0500

    NASA working

--- src/components/Jeremy/NASA.js
+++ src/components/Jeremy/NASA.js
@@ -1,3 +1,5 @@
+//ITERATION 1 - doesn't work
+
 // import { useEffect, useState } from "react";
 
 // import "./NASA.css";
@@ -32,32 +34,102 @@
 // };
 
 // export default Nasa;
-import React from 'react';
 
-const Nasa = (props) => {
+//ITERATION 2 - still doesn't work
+// Math.round(props.longitude * 100) / 100;
+// Math.round(props.latitude * 100) / 100;
+// import React from 'react';
+
+// const Nasa = (props) => {
 
     
-    const baseUrl = 'https://api.nasa.gov/planetary/earth/imagery';
-    const key = 'P0MkBcEQsjdw6xfNvmABK2c1OS2PpusY4PGSGkty';
-    const date = "2021-02-07";
-    const longitude = Math.round(props.longitude * 100) / 100;
-    const latitude = Math.round(props.latitude * 100) / 100;
-    const dim = .10;
+//     const baseUrl = 'https://api.nasa.gov/planetary/earth/imagery';
+//     const key = 'P0MkBcEQsjdw6xfNvmABK2c1OS2PpusY4PGSGkty';
+//     const date = "2020-10-10";
+//     const longitude = "86.1581";
+//     const latitude = "39.7684";
+//     const dim = .15;
     
-    const url = `${baseUrl}?lon=${longitude}&lat=${latitude}&date=${date}&dim=${dim}&api_key=${key}`;
+//     const url = `${baseUrl}?lon=${longitude}&lat=${latitude}&date=${date}&dim=${dim}&api_key=${key}`;
     
-    console.log(`NASA API url: ${url}`)
+//     console.log(`NASA API url: ${url}`);
 
-    return ( 
-        <>
-        <p>Hello from NASA!</p>
-        {latitude ? <span>{`Latitude is: ${props.latitude}`}</span> : <span>{`Latitude is: Locating...`}</span>}
-        <br />
-        {longitude ? <span>{`Longitude is: ${props.longitude}`}</span>: <span>{`Longitude is: Locating...`}</span>}
-        <br />
-        {longitude ? <img src={url} width="300" /> : <></>}
-        </>
-     );
-}
+//     return ( 
+//         <>
+//         <p>Hello from NASA!</p>
+//         {latitude ? <span>{`Latitude is: ${props.latitude}`}</span> : <span>{`Latitude is: Locating...`}</span>}
+//         <br />
+//         {longitude ? <span>{`Longitude is: ${props.longitude}`}</span>: <span>{`Longitude is: Locating...`}</span>}
+//         <br />
+//         {longitude ? <img src={url} width="300" /> : <></>}
+//         </>
+//      );
+// }
  
+// export default Nasa;
+
+//ITERATION THREE - still not working
+// import { useState,useEffect } from "react";
+// const Nasa=(props)=>{
+//   const base_url="https://api.nasa.gov/planetary/earth/imagery";
+//   const api_key="P0MkBcEQsjdw6xfNvmABK2c1OS2PpusY4PGSGkty"
+//   let d = new Date();
+//   const YYYY=d.getFullYear()-1;
+//   const MM=d.getMonth()+1<10?`0${d.getMonth()+1}`:`${d.getMonth()+1}`;
+//   const DD=d.getDate()<10?`0${d.getDate()}`:`${d.getDate()}`;
+//   const date=YYYY+"-"+MM+"-"+DD;
+//   console.log(props);
+//   const [image,setImage]=useState("");
+//   useEffect(()=>{
+//         const fetchImage = async()=>{
+//         let lon=await props.location.longitude;
+//         let lat=await props.location.latitude;
+//         setImage(`${base_url}?lon=${lon}&lat=${props.location.latitude}&date=${date}&dim=0.15&api_key=${api_key}`);
+//       }
+//       fetchImage();
+//     },[])  
+//   return(
+//       <div className="nasa">
+//           <h1>{props.location.city}, {props.location.region} one year ago today:</h1>
+//           <p>(Image is from one year ago to ensure an image exists)</p>
+//           <img src={props.url}/>
+//       </div>
+//   )
+// }
+// export default Nasa;
+
+//ITERATION FOUR
+import React, { useEffect, useState } from "react";
+
+const key = "P0MkBcEQsjdw6xfNvmABK2c1OS2PpusY4PGSGkty";
+const base_url = "https://api.nasa.gov/planetary/earth/assets";
+
+const Nasa = () => {
+  const [photoData, setPhotoData] = useState();
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+
+      fetch(
+        `${base_url}?lon=${long}&lat=${lat}&date=2018-02-01&&dim=0.15&api_key=${key}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setPhotoData(data.url);
+        });
+    });
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h1>What the aliens see</h1>
+        <img className="nasaImg" width="300" src={photoData} alt="NASA satellite image" />
+      </div>
+    </div>
+  );
+};
+
 export default Nasa;
